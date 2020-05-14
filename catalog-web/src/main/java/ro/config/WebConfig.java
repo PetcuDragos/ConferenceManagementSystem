1c377fe22c8e76060ee1c6020ac90e88e42164d7
dyenigma <dyenigma@gmail.com>
2020-05-14 15:01:31 +0300

    merged with feature/angular

--- catalog-web/src/main/java/ro/config/WebConfig.java
+++ catalog-web/src/main/java/ro/config/WebConfig.java
@@ -23,8 +23,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:4200", "http://localhost:8080")
-                        .allowedOrigins("http://localhost:4200", "http://localhost:8081")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:8080")
+
                         .allowedMethods("GET", "PUT", "POST", "DELETE");
             }
         };
